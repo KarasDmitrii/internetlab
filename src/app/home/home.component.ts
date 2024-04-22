86ecb57a1780f4bd2bb0de7c6c7c572de811b363
Dmitry Karas <dimakaras83@gmail.com>
2024-04-22 04:10:47 +0400

    add accordion block & font

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { IlHeaderComponent } from "../shared/header/header.component";
-import {HowItListComponent} from "../shared/how-it-list/howItList.component";
-import {StatisticComponent} from "../shared/statistic/statistic.component";
+import { HowItListComponent } from "../shared/how-it-list/howItList.component";
+import { StatisticComponent } from "../shared/statistic/statistic.component";
+import { AccordionComponent } from "../shared/accordion/accordion.component";
 
 
 @Component({
@@ -10,9 +11,34 @@ import {StatisticComponent} from "../shared/statistic/statistic.component";
   imports: [
     IlHeaderComponent,
     HowItListComponent,
-    StatisticComponent
+    StatisticComponent,
+    AccordionComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class IlHomeComponent { }
+export class IlHomeComponent {
+
+  protected accordion = {
+    heading: 'Вопросы и ответы',
+    items: [{
+      heading: 'Подтверждено: сознание наших соотечественников не замутнено пропагандой?',
+      content: `И по сей день в центральных регионах звучит перекатами печальный плач оппозиции?`
+    }, {
+      heading: 'Прототип нового сервиса - это как трубный призыв?',
+      content: `И по сей день в центральных регионах звучит перекатами печальный плач оппозиции?`
+    }, {
+      heading: 'Частокол на границе продолжает удивлять?',
+      content: `И по сей день в центральных регионах звучит перекатами печальный плач оппозиции?`
+    }, {
+      heading: 'Очевидцы сообщают, что слышали грохот грома градущих изменений?',
+      content: `И по сей день в центральных регионах звучит перекатами печальный плач оппозиции?`
+    }, {
+      heading: 'Нынче никто не может себе позволить инициировать треск разлетающихся скреп?',
+      content: `И по сей день в центральных регионах звучит перекатами печальный плач оппозиции?`
+    }, {
+      heading: 'Высококачественный прототип будущего проекта обнадёживает?',
+      content: `И по сей день в центральных регионах звучит перекатами печальный плач оппозиции?`
+    }]
+  }
+}
---
+++ src/app/shared/accordion/accordion.component.ts
@@ -0,0 +1,23 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { NgForOf, NgIf } from "@angular/common";
+import {AccordionItemComponent} from "./accordion-item/accordion-item.component";
+
+
+@Component({
+  selector: 'il-accordion',
+  templateUrl: "accordion.component.html",
+  styleUrl: "accordion.component.scss",
+  standalone: true,
+  imports: [
+    NgForOf,
+    NgIf,
+    AccordionItemComponent,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AccordionComponent {
+
+  @Input() heading: string;
+  @Input() items: any;
+
+}
